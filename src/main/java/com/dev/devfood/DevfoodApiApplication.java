f07874476a768af398e3c7f3eb7d9cddc07789eb
Paulo Nogueira <paulobh.nogueira@gmail.com>
2021-09-23 21:45:36 -0300

    Commit inicial - Criação do projeto.

---
+++ src/main/java/com/dev/devfood/DevfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.devfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevfoodApiApplication.class, args);
+	}
+
+}
