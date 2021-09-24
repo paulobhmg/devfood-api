4945677eeb3b776e57875fe43cdae099c9a4c6ce
Paulo Nogueira <paulobh.nogueira@gmail.com>
2021-09-24 07:39:49 -0700

    Endpoint findById para controllers de cozinha, restaurante e estado.

--- src/main/java/com/dev/devfood/api/controller/EstadoController.java
+++ src/main/java/com/dev/devfood/api/controller/EstadoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class EstadoController {
 	@GetMapping
 	public List<Estado> list(){
 		return repository.list();		
+	}
+	
+	@GetMapping("/{id}")
+	public Estado findById(@PathVariable Long id) {
+		return repository.findById(id);
 	}
 }
